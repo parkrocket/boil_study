081deadde5261b1e4976fd508076e7552a03d72f
parkrocket <shinbyro@kakao.kr>
2023-03-28 18:04:18 +0900

    로그인 파트 커밋

--- src/Comp/View/Login/Login.js
+++ src/Comp/View/Login/Login.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Login() {
-    return <div>로그인</div>;
+    return (
+        <div>
+            <form>
+                <div>
+                    <input type="text" name="id" value=""></input>
+                </div>
+                <div>
+                    <input type="password" name="password" value=""></input>
+                </div>
+                <div>
+                    <input type="submit" value="로그인하기"></input>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
