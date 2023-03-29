297b7aedae9c6b466b2322e30cfb69c9741b9dc5
parkrocket <shinbyro@kakao.kr>
2023-03-29 16:02:09 +0900

    로그인 서버 통신 성공

--- src/Comp/View/Login/Login.js
+++ src/Comp/View/Login/Login.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Login() {
+    const [Id, setId] = useState('');
+    const [Password, setPassword] = useState('');
+
+    const onIdHandler = (event) => {
+        setId(event.currentTarget.value);
+    };
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    };
+
+    const onSubmitHandler = (event) => {
+        const data = { id: Id, password: Password };
+
+        console.log(data);
+
+        axios({
+            method: 'post',
+            url: 'http://54.180.35.70/api/users',
+            data: data,
+        }).then((response) => console.log(response));
+
+        event.preventDefault();
+    };
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmitHandler}>
                 <div>
-                    <input type="text" name="id" value=""></input>
+                    <input type="text" name="id" defaultValue="" onChange={onIdHandler}></input>
                 </div>
                 <div>
-                    <input type="password" name="password" value=""></input>
+                    <input type="password" name="password" defaultValue="" onChange={onPasswordHandler}></input>
                 </div>
                 <div>
-                    <input type="submit" value="로그인하기"></input>
+                    <input type="submit" defaultValue="로그인하기"></input>
                 </div>
             </form>
         </div>
