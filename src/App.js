aa9d73c02a3031e8da4f3adf3b6ec9ed3b42ee0e
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2023-03-29 23:58:02 +0900

    [chore] typescript 추가 (#1)

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Register from './Comp/View/Register/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChakraBaseProvider } from '@chakra-ui/react';
 
+import Test from './Comp/Test';
+
 function App() {
-    return (
-        <ChakraBaseProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/introduce" element={<Introduce />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Routes>
-            </Router>
-        </ChakraBaseProvider>
-    );
+  return (
+    <ChakraBaseProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/introduce' element={<Introduce />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/test' element={<Test value='테스트 컴포넌트 :)😁' />} />
+        </Routes>
+      </Router>
+    </ChakraBaseProvider>
+  );
 }
 
 export default App;
