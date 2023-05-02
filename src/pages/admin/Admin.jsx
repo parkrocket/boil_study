f212c2c7c67ee4300369eac26897a2f68ab35ddc
parkrocket <shinbyro@kakao.kr>
2023-05-02 17:42:36 +0900

    관리자 라우터, 마이페이지 라우터 생성

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Head from "../../components/Head";
 
 function Admin() {
-    return <div>Admin</div>;
+    return (
+        <div>
+            <Head></Head>
+            <div className="container_wrap">Admin</div>
+        </div>
+    );
 }
 
 export default Admin;
