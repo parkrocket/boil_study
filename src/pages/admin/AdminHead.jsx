7f2e89102edc3df70b20f841f76954c0bd5d33d8
daazzo <sepen27@naver.com>
2023-05-12 14:57:29 +0900

    리자몽 추가, admin 작업

--- src/pages/admin/AdminHead.jsx
+++ src/pages/admin/AdminHead.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import adminStyle from '../../Css/admin.module.scss';
 import { Avatar } from "@chakra-ui/react";
 import LogoutIcon from '@mui/icons-material/Logout';
+import MenuIcon from '@mui/icons-material/Menu';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 
 function AdminHead() {
@@ -9,20 +11,20 @@ function AdminHead() {
         <div className={`${adminStyle.admin_head}`}>
             <div className={`${adminStyle.container}`}>
                 <div className={`${adminStyle.left}`}>
-                    <a href="" className={`${adminStyle.ham_btn}`}>ham</a>
+                    <a href="" className={`${adminStyle.ham_btn}`}><MenuIcon/></a>
                 </div>
                 <div className={`${adminStyle.right}`}>
                     <ul className={`${adminStyle.util}`}>
                         <li>
                             <ul className={`${adminStyle.util_sub}`}>
                                 <li><a href="">관리자</a></li>
-                                <li><a href="">무료모집9946명</a></li>
+                                <li><a href="">무료모집</a></li>
                             </ul>
                         </li>
                         <li>
                             <div className={`${adminStyle.profile}`}>
                                 <Avatar className={`${adminStyle.img}`}></Avatar>
-                                <p>nickName</p>
+                                <p><span>nickName</span><ArrowDropDownIcon/></p>
                             </div>
                             <ul className={`${adminStyle.profile_sub}`}>
                                 <li><a href="">쿠폰</a></li>
