d1d50b9dabeee4ee10df709554fdfbbbadee5abe
daazzo <sepen27@naver.com>
2023-05-12 17:50:58 +0900

    admin 수정

--- src/pages/admin/AdminHead.jsx
+++ src/pages/admin/AdminHead.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import adminStyle from '../../Css/admin.module.scss';
 import { Avatar } from "@chakra-ui/react";
 import LogoutIcon from '@mui/icons-material/Logout';
 import MenuIcon from '@mui/icons-material/Menu';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { NavLink } from "react-router-dom";
 
 
 function AdminHead() {
+
+    const [nickNameClick, setNickNameClick] = useState(false);
+    const [gnbMenuClick, setgnbMenuClick] = useState(false);
+
+    const handleNickNameClick = () => {
+        setNickNameClick(!nickNameClick);
+        console.log(nickNameClick);
+    };
+
     return (
         <div className={`${adminStyle.admin_head}`}>
             <div className={`${adminStyle.container}`}>
@@ -17,21 +27,21 @@ function AdminHead() {
                     <ul className={`${adminStyle.util}`}>
                         <li>
                             <ul className={`${adminStyle.util_sub}`}>
-                                <li><a href="">관리자</a></li>
-                                <li><a href="">무료모집</a></li>
+                                <li><NavLink to="/admin">관리자</NavLink></li>
+                                <li><NavLink to="/free">무료모집</NavLink></li>
                             </ul>
                         </li>
                         <li>
-                            <div className={`${adminStyle.profile}`}>
+                            <div className={`${adminStyle.profile}`} onClick={handleNickNameClick}>
                                 <Avatar className={`${adminStyle.img}`}></Avatar>
-                                <p><span>nickName</span><ArrowDropDownIcon/></p>
+                                <p className={`${nickNameClick ? adminStyle.active : ""}`}><span>nickName</span><ArrowDropDownIcon/></p>
                             </div>
-                            <ul className={`${adminStyle.profile_sub}`}>
-                                <li><a href="">쿠폰</a></li>
-                                <li><a href="">광고주 정보수정</a></li>
-                                <li><a href="">고객센터</a></li>
-                                <li><a href="">오늘리뷰</a></li>
-                                <li><a href="">로그아웃<LogoutIcon/></a></li>
+                            <ul className={`${adminStyle.profile_sub} ${nickNameClick ? adminStyle.active : ""}`}>
+                                <li><NavLink to="">쿠폰</NavLink></li>
+                                <li><NavLink to="">광고주 정보수정</NavLink></li>
+                                <li><NavLink to="">고객센터</NavLink></li>
+                                <li><NavLink to="">오늘리뷰</NavLink></li>
+                                <li><NavLink to="">로그아웃<LogoutIcon/></NavLink></li>
                             </ul>
                         </li>
                     </ul>
